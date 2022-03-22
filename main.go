4d5aa9a983a3ec0214e01382af0f7ffcd8ab78e7
charconstpointer <attwistingnether@gmail.com>
2022-03-22 11:10:57 +0100

    feat: add notes to measurements

--- main.go
+++ main.go
@@ -4,19 +4,41 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: forma <weight> <note (optional)>")
+		os.Exit(1)
+	}
+
 	args := os.Args[1:]
 	cmd := args[0]
 	switch cmd {
 	case "add":
-		add(time.Now(), args[1])
+		var note string
+		if len(args) > 2 {
+			note = strings.Join(args[2:], " ")
+		}
+
+		m := &Measurement{
+			Weight: args[1],
+			Date:   time.Now(),
+			Note:   note,
+		}
+		add(m)
 	}
 }
 
-func add(date time.Time, weight string) error {
+type Measurement struct {
+	Date   time.Time
+	Weight string
+	Note   string
+}
+
+func add(m *Measurement) error {
 	f, err := os.OpenFile("weight.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return fmt.Errorf("failed to open weight.csv, %w", err)
@@ -24,7 +46,7 @@ func add(date time.Time, weight string) error {
 
 	wr := csv.NewWriter(f)
 	defer wr.Flush()
-	err = wr.Write([]string{date.Format(time.RFC3339), weight})
+	err = wr.Write([]string{m.Date.Format(time.RFC3339), m.Weight, m.Note})
 	if err != nil {
 		return fmt.Errorf("failed to write weight, %w", err)
 	}
